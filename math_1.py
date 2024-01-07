9e1a18e8a6eb9d4d013b5bd360548823a5ebd420
hiroky1983 <hirockysan1983@gmail.com>
2024-01-08 01:12:51 +0900

    pythonの基礎、数学編

--- math_1.py
+++ math_1.py
@@ -1,12 +1,26 @@
 # 関数の描画
 ## NumPyの変換
 import numpy as np # NumPyをインポート
+import matplotlib.pyplot as plt # matplotlib.pyplotをインポート
 
 a = [0, 1, 2, 3, 4, 5] # リストaを定義
 b = np.array(a) # NumPy配列に変換
 print(b) # bを表示
 
+#linespace
+x = np.linspace(-5,5) # -5から5までの範囲を50分割したNumPy配列を生成
+
+print(x)
+print(len(x))
+
 ##１次関数
+######数式の練習
+# $$y=2x+1$$
+
+x = np.linspace(-5,5)
+y = 2*x + 1
+
+plt.plot(x,y)
 
 ## グラフの装飾
 
