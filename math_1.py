feee5a5c63ce2f19b55b8c8256aed51e7d7ef22a
hiroky1983 <hirockysan1983@gmail.com>
2024-01-08 13:37:57 +0900

    二次関数、三次関数

--- math_1.py
+++ math_1.py
@@ -25,4 +25,34 @@ plt.show()
 
 ## グラフの装飾
 
+x = np.linspace(-3,3)
+y_1 = 1.5*x
+y_2 = -2*x+1
+
+plt.xlabel("x value", size=14)
+plt.ylabel("y value", size=14)
+
+plt.title("my graph")
+plt.grid()
+
+plt.plot(x,y_1, label="y_1")
+plt.plot(x,y_2, label="y_2", linestyle="dashed")
+
+plt.legend()
+plt.show()
+
 ##二次関数、三次関数
+x = np.linspace(-4,4)
+y_1 = 2*x +1
+y_2 = x**2 -4
+y_3 = 0.5*x**3 -6*x
+
+plt.plot(x, y_1, label="1st")
+plt.plot(x, y_2, label="2nd")
+plt.plot(x, y_3, label="3rd")
+plt.legend()
+
+plt.xlabel("x", size=14)
+plt.ylabel("y", size=14)
+plt.grid()
+plt.show()
